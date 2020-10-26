f11bb5651be72d916694e7dd3bdcb626ea26f335
AlFlores10 <alberto.flores.1990@gmail.com>
2020-10-26 07:00:45 +0100

    arrayPeliculas, endpoint users, films y pedidos creados

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express'); // IMPORTA EXPRESS
-const cors = require('cors');
-const app = express();
-app.use(cors()); // USO CORS
+const cors = require('cors'); // IMPORTA CORS
+const app = express(); // USA EXPRESS
+app.use(cors()); // USA CORS
 
 
 
@@ -11,10 +11,38 @@ let result = double(3);
 console.log(result);
 console.log("hola mundo", process.argv[2]);
 
+// -----------------------------------------------------------------------
+const carteleraPeliculas = [
+    { id: 1, titulo: 'Matrix'},
+    { id: 2, titulo: 'Spartacus'},
+    { id: 3, titulo: 'Los juegos del hambre'},
+    { id: 4, titulo: 'Monstruos S.A.'},
+]
+
+// --------------------------------------------------------------------
+
 app.listen(3000, ()=> { // INICIANDO SERVIDOR NODE
     console.log('Servidor levantado en 3000 ms');
 });
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res)=> { // PAGINA DE INICIO
     res.send('<h1>proy.Videoclub-API Version 1.0.0</h1>')
 });
+
+
+app.get('/users', (req, res)=> { // PAGINA DE USUARIOS
+    res.send('USUARIOS')
+});
+
+
+app.get('/films', (req, res)=> { // PAGINA DE USUARIOS
+    res.send('FILMS')
+});
+
+
+app.get('/pedidos', (req, res)=> { // PAGINA DE USUARIOS
+    res.send('PEDIDOS')
+});
+
+
+// -----------------------------------------------------------------------------
