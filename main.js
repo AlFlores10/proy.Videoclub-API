189850a59e445e842b3b9084308c281fd225f6f1
AlFlores10 <alberto.flores.1990@gmail.com>
2020-10-26 06:33:43 +0100

    cors añadido y endpoint principal

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
+const express = require('express'); // IMPORTA EXPRESS
+const cors = require('cors');
+const app = express();
+app.use(cors()); // USO CORS
+
+
+
 let double = require('./modules.js')
 let result = double(3);
 
 console.log(result);
 console.log("hola mundo", process.argv[2]);
 
-const express = require('express'); // IMPORTA EXPRESS
-const cors = require('cors');
-const app = express();
-app.use(cors()); // USO CORS
+app.listen(3000, ()=> { // INICIANDO SERVIDOR NODE
+    console.log('Servidor levantado en 3000 ms');
+});
+
+app.get('/', (req, res)=> {
+    res.send('<h1>proy.Videoclub-API Version 1.0.0</h1>')
+});
